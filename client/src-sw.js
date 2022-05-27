8c6cb07ff5eebf25602753e6fa6b3f0c7d5010f0
Jake Walsh <jkwalsh127@gmail.com>
2022-05-26 18:34:50 -0700

    refactor src-sw to original

--- client/src-sw.js
+++ client/src-sw.js
@@ -7,27 +7,27 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-//CacheFirst relies on the cache, falling back to network only if response is not available in cache. The network response will be cached for later use
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    // cache any requests with a status code between 0 and 200
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    // restricts caching responses with these headers to a max of 30 days
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-// recipe to load provided URLs during service worker's install. 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+// //CacheFirst relies on the cache, falling back to network only if response is not available in cache. The network response will be cached for later use
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     // cache any requests with a status code between 0 and 200
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     // restricts caching responses with these headers to a max of 30 days
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
+// // recipe to load provided URLs during service worker's install. 
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
 
-registerRoute(({ request }) => request.mode === "navigate", pageCache);
+// registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 registerRoute(
   // matching function to determine if the route should be a match to the request

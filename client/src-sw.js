6a7ee4a8ab46ea66848d3c53cdd1303e556ea1fd
Jake Walsh <jkwalsh127@gmail.com>
2022-05-26 19:33:39 -0700

    cleanup

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,33 +1,9 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
-
-// //CacheFirst relies on the cache, falling back to network only if response is not available in cache. The network response will be cached for later use
-// const pageCache = new CacheFirst({
-//   cacheName: 'page-cache',
-//   plugins: [
-//     // cache any requests with a status code between 0 and 200
-//     new CacheableResponsePlugin({
-//       statuses: [0, 200],
-//     }),
-//     // restricts caching responses with these headers to a max of 30 days
-//     new ExpirationPlugin({
-//       maxAgeSeconds: 30 * 24 * 60 * 60,
-//     }),
-//   ],
-// });
-// // recipe to load provided URLs during service worker's install. 
-// warmStrategyCache({
-//   urls: ['/index.html', '/'],
-//   strategy: pageCache,
-// });
-
-// registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 registerRoute(
   // matching function to determine if the route should be a match to the request
